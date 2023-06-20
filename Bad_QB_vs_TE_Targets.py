4f833d7859d8199c9cca4702d064bea28e3f6d80
JanCalalo <97128826+JanCalalo@users.noreply.github.com>
2023-06-19 20:48:08 -0400

    creation of badqbvstarget

--- Bad_QB_vs_TE_Targets.py
+++ Bad_QB_vs_TE_Targets.py
@@ -13,7 +13,27 @@ import scipy.optimize as opti
 #%% Load Weekly  TE targets
 
 
+
+get_pbp_cols = nfl.see_pbp_cols()
+
+pbp_cols = ['week','play_id','play_type','passer_player_name','receiver_player_name','passer_id','receiver_id','pass_length','sack','pass_attempt']
+years = [2022]
+
+current_pbp_data = nfl.import_pbp_data(years,pbp_cols)
+
+
+
 #%% sync to weekly targets
+
+
+unique_games = current_pbp_data.nflverse_game_id.unique()
+
+
+#%%
+selected_game = unique_games[0]
+select_pbp_game = current_pbp_data[current_pbp_data['nflverse_game_id'] == selected_game]
+
+selected_pbp_game_pass = select_pbp_game[select_pbp_game['play_type'] == 'pass']
 
 #%% 
 ngs_data = nfl.import_ngs_data('passing',[2022])
