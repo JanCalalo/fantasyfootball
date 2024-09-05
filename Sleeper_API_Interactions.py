4e6a83699407b77e28f68a7ad285bc2f67bb44e9
JanCalalo <97128826+JanCalalo@users.noreply.github.com>
2024-09-05 03:00:46 +0000

    Sep 4, 2024, 11:00 PM

---
+++ Sleeper_API_Interactions.py
@@ -0,0 +1,20 @@
+"""
+Get Player Names from Sleeper
+
+For Upload to Google Drive
+"""
+
+
+import requests
+import os
+
+if True:
+    player_list= requests.get("https://api.sleeper.app/v1/players/nfl").json()
+
+
+player_list_dict = pd.DataFrame.from_dict(player_list,orient='index')
+active_players_sleeper_dataframe = player_list_dict[player_list_dict['status'] != 'Inactive']
+
+
+curr_folder = os.getcwd()
+active_players_sleeper_dataframe.to_csv(os.path.join(curr_folder,'Local_Files','2024_Players.csv'))
